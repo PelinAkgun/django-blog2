5f3ee7ce63f785aafa4779d9d11172c6884589c9
Pelin Akgün <pelinakgun1999@gmail.com>
2023-07-09 18:08:04 +0300

    iletisim modeli admin paneli ayarlandı

--- blog/admin.py
+++ blog/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 from blog.models import KategoriModel
 from blog.models import YazılarModel
-
+from blog.models import YorumModel
+from blog.models import IletisimModel
 # Register your models here.
 admin.site.register(KategoriModel)
 
@@ -11,3 +12,16 @@ class YazilarAdmin(admin.ModelAdmin):
         'baslik','olusturulma_tarihi','duzenlenme_tarihi'
     )
 admin.site.register(YazılarModel, YazilarAdmin)
+
+class YorumlarAdmin ( admin.ModelAdmin):
+    search_fields =('yazan__username' , )
+    list_display = (
+        'yorum' , 'olusturulma_tarihi' , 'guncellenme_tarihi'
+    ) 
+admin.site.register(YorumModel, YorumlarAdmin)
+
+class IletisimAdmin(admin.ModelAdmin):
+    search_fields = ('email' , )
+    list_display = (
+        'email','olusturulma_tarihi')
+admin.site.register(IletisimModel, IletisimAdmin)
--- blog/models/__init__.py
+++ blog/models/__init__.py
@@ -1,2 +1,4 @@
 from .kategori import KategoriModel
 from .yazi import YazılarModel
+from .yorum import YorumModel
+from .iletisim import IletisimModel
